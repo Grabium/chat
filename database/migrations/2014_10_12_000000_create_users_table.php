1ec87190af0dd9d8e14cdf8f324b690a68deae89
Grabium <110872805+Grabium@users.noreply.github.com>
2026-01-18 02:50:59 -0300

    Migrações feitas. Docker-compose: foi comentada a linha 'comand ...' que era de uma versão obsoleta do mysql

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('language');
+            $table->string('channel', 50)->unique();
+            $table->timestamp('last_seen')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
